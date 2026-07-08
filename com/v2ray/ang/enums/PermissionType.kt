72e449bd690a10f248b8d79418800c6f3b9cca08
hamedp71 <170614544+hamedp-71@users.noreply.github.com>
2026-07-08 18:22:07 +0330

    Add files via upload

---
+++ com/v2ray/ang/enums/PermissionType.kt
@@ -0,0 +1,36 @@
+package com.v2ray.ang.enums
+
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+enum class PermissionType {
+    
+    CAMERA {
+        override fun getPermission(): String = Manifest.permission.CAMERA
+    },
+
+    
+    POST_NOTIFICATIONS {
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        override fun getPermission(): String = Manifest.permission.POST_NOTIFICATIONS
+    },
+
+    
+    ACCESS_LOCAL_NETWORK {
+        @RequiresApi(Build.VERSION_CODES.CINNAMON_BUN)
+        override fun getPermission(): String = Manifest.permission.ACCESS_LOCAL_NETWORK
+    };
+
+    
+    abstract fun getPermission(): String
+
+    
+    fun getLabel(): String {
+        return when (this) {
+            CAMERA -> "Camera"
+            POST_NOTIFICATIONS -> "Notification"
+            ACCESS_LOCAL_NETWORK -> "Local Network"
+        }
+    }
+}

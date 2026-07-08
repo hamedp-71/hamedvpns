0415ab9f98bcd7d696121e9560c3ac4186b785dc
hamedp71 <170614544+hamedp-71@users.noreply.github.com>
2026-07-08 17:15:45 +0330

    Add files via upload

---
+++ com/v2ray/ang/contracts/BaseAdapterListener.kt
@@ -0,0 +1,16 @@
+package com.v2ray.ang.contracts
+
+interface BaseAdapterListener {
+    
+    fun onEdit(guid: String, position: Int)
+
+    
+    fun onRemove(guid: String, position: Int)
+
+    
+    fun onShare(url: String)
+
+    
+    fun onRefreshData()
+}
+
---
+++ com/v2ray/ang/contracts/Tun2SocksControl.kt
@@ -0,0 +1,10 @@
+package com.v2ray.ang.contracts
+
+interface Tun2SocksControl {
+    
+    fun startTun2Socks()
+
+    
+    fun stopTun2Socks()
+}
+

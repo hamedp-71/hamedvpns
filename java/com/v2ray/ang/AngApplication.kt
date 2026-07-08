b9580ea8896ced6b096fb99da112dbf8d034418a
hamedp71 <170614544+hamedp-71@users.noreply.github.com>
2026-07-08 18:23:42 +0330

    Add files via upload

---
+++ java/com/v2ray/ang/AngApplication.kt
@@ -0,0 +1,42 @@
+package com.v2ray.ang
+
+import android.content.Context
+import androidx.multidex.MultiDexApplication
+import androidx.work.Configuration
+import androidx.work.WorkManager
+import com.tencent.mmkv.MMKV
+import com.v2ray.ang.AppConfig.ANG_PACKAGE
+import com.v2ray.ang.handler.SettingsManager
+
+class AngApplication : MultiDexApplication() {
+    companion object {
+        lateinit var application: AngApplication
+    }
+
+    
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        application = this
+    }
+
+    private val workManagerConfiguration: Configuration = Configuration.Builder()
+        .setDefaultProcessName("${ANG_PACKAGE}:bg")
+        .build()
+
+    
+    override fun onCreate() {
+        super.onCreate()
+
+        MMKV.initialize(this)
+
+        WorkManager.initialize(this, workManagerConfiguration)
+
+        SettingsManager.initApp(this)
+        SettingsManager.setNightMode()
+
+        es.dmoral.toasty.Toasty.Config.getInstance()
+            .setGravity(android.view.Gravity.BOTTOM, 0, 300)
+            .apply()
+    }
+}
+

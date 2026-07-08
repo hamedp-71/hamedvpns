9e2071415d8a87e4afe28f15cb8dee286142538f
hamedp71 <170614544+hamedp-71@users.noreply.github.com>
2026-07-08 18:25:52 +0330

    Add files via upload

---
+++ java/com/v2ray/ang/dto/RealPingEvent.kt
@@ -0,0 +1,15 @@
+package com.v2ray.ang.dto
+
+sealed class RealPingEvent {
+
+    
+    data class Progress(val text: String) : RealPingEvent()
+
+    
+    data class Result(val guid: String, val delayMillis: Long) : RealPingEvent()
+
+    
+    data class Finish(val status: String) : RealPingEvent()
+}
+
+
